e9b73d0c59cd63c017cf485b57ec76f290f6f160
Moloy Das <moloy.bitspilani@gmail.com>
2020-09-17 18:06:15 +0530

    Synthetic image generator generates large displacement images as well

--- syn_img.py
+++ syn_img.py
@@ -3,14 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = np.ones((200,200)) * 255
-
 img = cv2.rectangle(img, (50,50), (100,100), (0,0,0), -1)
-
-plt.imshow(img)
-plt.savefig("img_1.jpg")
+cv2.imwrite("./test_images/img_1.jpg", img)
 
 img = np.ones((200,200)) * 255
 img = cv2.rectangle(img, (52,52), (102,102), (0,0,0), -1)
+cv2.imwrite("./test_images/img_2.jpg", img)
 
-plt.imshow(img)
-plt.savefig("img_2.jpg")
+img = np.ones((200,200)) * 255
+img = cv2.rectangle(img, (50,50), (100,100), (0,0,0), -1)
+cv2.imwrite("./test_images/img_1_large_disp.jpg", img)
+
+img = np.ones((200,200)) * 255
+img = cv2.rectangle(img, (55,55), (105 ,105), (0,0,0), -1)
+cv2.imwrite("./test_images/img_2_large_disp.jpg", img)
